383376d7b8c06ad695e5cc9320f728490ac238ae
isak89 <isakimamverdiyev@gmail.com>
2022-04-08 15:26:35 +0400

    cs

---
+++ subnets.tf
@@ -0,0 +1,65 @@
+resource "aws_subnet" "public_1" {
+    
+    vpc_id = aws_vpc.main.id 
+    
+    cidr_block = "10.0.0.0/18"
+    
+    availability_zone = "us-west-1b"
+
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name                        = "public-us-west-1b"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb"    = 1
+    }
+
+}
+
+resource "aws_subnet" "public_2" {
+    
+    vpc_id = aws_vpc.main.id 
+    
+    cidr_block = "10.0.64.0/18"
+    
+    availability_zone = "us-west-1c"
+
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name                        = "public-us-west-1c"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb"    = 1
+    }
+}
+
+resource "aws_subnet" "private_1" {
+    
+    vpc_id = aws_vpc.main.id 
+    
+    cidr_block = "10.0.128.0/18"
+    
+    availability_zone = "us-west-1b"
+
+    tags = {
+        Name                        = "private-us-west-1b"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb"    = 1
+    }
+
+}
+resource "aws_subnet" "private_2" {
+    
+    vpc_id = aws_vpc.main.id 
+    
+    cidr_block = "10.0.192.0/18"
+    
+    availability_zone = "us-west-1c"
+
+    tags = {
+        Name                        = "private-us-west-1c"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb"    = 1
+    }
+
+}
